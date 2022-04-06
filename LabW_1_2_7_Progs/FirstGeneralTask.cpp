6ae660d2af433cb3204f5bb5ed12a2f9ba2864e4
Александр Немеров <nemerov_a@mail.ru>
2022-04-06 12:43:00 +0800

    jghjg

--- LabW_1_2_7_Progs/FirstGeneralTask.cpp
+++ LabW_1_2_7_Progs/FirstGeneralTask.cpp
@@ -17,7 +17,53 @@ enum class Massive_Menu
 class IntSetMassive
 {
 
+private:
 
+	IntSetMassive* next = NULL;
+	IntSetMassive* next = NULL;
+
+	class Mass
+	{
+
+	private:
+
+		Mass* next = NULL;
+		int data;
+
+	public:
+		Mass()
+		{
+
+		}
+
+		Mass(int val)
+		{
+
+			this->data = val;
+			next = new Mass();
+		}
+	}Arr;
+
+	Mass* head = NULL;
+
+public:
+
+	IntSetMassive()
+	{
+		Arr(0);
+	}
+
+	IntSetMassive(int *val)
+	{
+		int size_arr = sizeof(val) / sizeof(val[0]);
+		
+	}
+
+
+	~IntSetMassive()
+	{
+
+	}
 };
 
 void firstGeneralTask()
