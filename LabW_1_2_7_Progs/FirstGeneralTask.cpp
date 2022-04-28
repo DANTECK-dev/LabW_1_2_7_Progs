75de1bce173069e1c8e72ad041689b3488726745
Александр Немеров <nemerov_a@mail.ru>
2022-04-29 00:30:03 +0800

    15

--- LabW_1_2_7_Progs/FirstGeneralTask.cpp
+++ LabW_1_2_7_Progs/FirstGeneralTask.cpp
@@ -1,485 +1,195 @@
+
 #include "FirstGeneralTask.h"
+
 #include "ThisIs.h"
-#include <iostream>
-#include <conio.h>
-#include <string>
+
+#include "Libraries.h"
 
 using namespace std;
 
-enum class Massive_Menu
-{
-	Exit,
-	AddMass,
-	AddItem,
-	Combine,
-	Remove,
-	Intersection,
-	Return,
-	Size
-};
-
-class IntSetMassive
-{
-
+class IntSetMassive {
 public:
 
-	IntSetMassive(int* val, int arr_size);
+	IntSetMassive();
 	~IntSetMassive();
-	IntSetMassive(int val);
-	bool currentNext(IntSetMassive*& current_ISM);
-	bool currentPrev(IntSetMassive*& current_ISM);
-	void createMassive(IntSetMassive*& current_ISM, int* val, int arr_size);
-	void getMassive(IntSetMassive*& current_ISM, IntSetMassive*& head_ISM, IntSetMassive*& tail_ISM);
-	void operator+(const int value);
-	void operator+(const IntSetMassive& object);
-	void operator-(const int value);
-	void operator-(const IntSetMassive& object);
+	int getSize();
+	void pushBack(const int data);
+	void showAll();
+	void deleteNode(const int index);
+	void clearAll();
+	bool chekValue(const int value);
+	void operator+(const int data);
+	void operator+(const IntSetMassive& other);
+	void operator-(const int data);
+	void operator-(const IntSetMassive& other);
 	int& operator[](const int index);
-	int sizeMass(const IntSetMassive*& object);
+	int getIndexOfValue(const int value);
 
 private:
-
-	IntSetMassive* next_ISM = NULL;
-	IntSetMassive* prev_ISM = NULL;
-
-	struct Mass
-	{
-		Mass* next_M = NULL;
-		int data = 0;
-
-		Mass(int val) { data = val; }
-		~Mass() { delete next_M; }
+	class Node {
+	public:
+		int data;
+		Node* pNext;
+		Node(int data = 0, Node* pNext = nullptr) {
+			this->data = data;
+			this->pNext = pNext;
+		}
 	};
-
-	Mass* head_M = NULL;
-	Mass* current_M = NULL;
-
-	static bool first_ISM;
-	bool first_M = true;
+	int SIZE;
+	Node* pHead;
 };
 
-IntSetMassive::IntSetMassive(int val)
-{
-	if (first_M == true)
-	{
-		current_M = new Mass(val);
-		head_M = current_M;
-		first_M = false;
-	}
-
-	else
-	{
-		current_M->next_M = new Mass(val);
-		current_M = current_M->next_M;
-	}
+IntSetMassive::IntSetMassive() {
+	this->pHead = nullptr;
+	this->SIZE = 0;
 }
 
-IntSetMassive::IntSetMassive(int* val, int arr_size)
-{
-	/*int* temp = NULL;
-
-	for (int i = 0; i < arr_size; i++)
-	{
-		for (int j = (i + 1); j < arr_size; i++)
-		{
-			if (val[i] == val[j])
-			{
-				arr_size--;
-				temp = new int[arr_size];
-
-				for (int k = 0; k < arr_size; k++)
-				{
-					if (val[k] == val[i]) continue;
-					temp[k] = val[k];
-				}
-				//hjgvbm,n.m
-				for (int k = 0; k < arr_size; k++)
-					val[k] = temp[k];
-			}
-		}
-	}
-	*/
-	for (int i = 0; i < arr_size; i++)
-	{
-		if (first_M == true)
-		{
-			current_M = new Mass(val[i]);
-			head_M = current_M;
-			first_M = false;
-		}
-
-		else
-		{
-			current_M->next_M = new Mass(val[i]);
-			current_M = current_M->next_M;
-		}
-
-		//current_M->data = val[i];
-	}
+IntSetMassive::~IntSetMassive() {
+	clearAll();
 }
 
-IntSetMassive::~IntSetMassive()
-{
-	delete next_ISM;
-	delete prev_ISM;
-	delete head_M;
-	delete current_M;
+int IntSetMassive::getSize() {
+	return SIZE;
 }
 
-bool IntSetMassive::currentNext(IntSetMassive*& current_ISM)
-{
-	if (current_ISM->next_ISM == NULL)
-		return false;
-	else
-	{
-		current_ISM = current_ISM->next_ISM;
-		return true;
-	}
-}
-
-bool IntSetMassive::currentPrev(IntSetMassive*& current_ISM)
-{
-	if (current_ISM->prev_ISM == NULL)
-		return false;
-	else
-	{
-		current_ISM = current_ISM->prev_ISM;
-		return true;
-	}
-}
-
-void IntSetMassive::createMassive(IntSetMassive*& current_ISM, int* val, int arr_size)
-{
-	if (first_ISM == true)
-	{
-		first_ISM = false;
-		current_ISM = new IntSetMassive(val, arr_size);
-	}
-
-	else
-	{
-		current_ISM->next_ISM = new IntSetMassive(val, arr_size);
-		current_ISM = current_ISM->next_ISM;
-		current_ISM->first_ISM = false;
-	}
-}
-
-void IntSetMassive::getMassive(IntSetMassive *&current_ISM, IntSetMassive *&head_ISM, IntSetMassive *&tail_ISM)
-{
-	if (current_ISM == NULL && head_ISM == NULL && tail_ISM == NULL) { cout << "\n\tНечего выводить\n\t"; return; }
-
-	current_ISM = head_ISM;
-	int counter = 0;
-
-	while (true)
-	{
-		counter++;
-		cout << "\n\t" << counter << " множество = { ";
-
-		current_ISM->current_M = current_ISM->head_M;
-
-		while (true)
-		{
-			cout << current_ISM->current_M->data;
-
-			if (current_ISM->current_M->next_M == NULL) break;
-
-			cout << ", ";
-
-			current_ISM->current_M = current_ISM->current_M->next_M;
-		}
-
-		cout << " }";
-
-		if (current_ISM->next_ISM == NULL) break;
-		current_ISM = current_ISM->next_ISM;
-	}
-
-}
-	
-void IntSetMassive::operator+(const int value)
-{
-	Mass* TempMass = this->head_M;
-	/*if (pTemp == nullptr) {
-		this -> head_M = new Mass(value);
+void IntSetMassive::pushBack(const int data) {
+	if (this->pHead == nullptr) {
+		this->pHead = new Node(data);
 	}
 	else {
-		while (pTemp->next_M != nullptr) {
-
-			pTemp = pTemp->next_M;
+		Node* pTemp = this->pHead;
+		while (pTemp->pNext != nullptr) {
+			pTemp = pTemp->pNext;
 		}
-		pTemp->next_M = new Mass(value);
-	}*/
-
-	if (TempMass == NULL) cout << "\n\tЭтого множества не существует\n\t"; return;
-
-	while (true)
-	{
-		if (TempMass->next_M == NULL) break;
-		TempMass = TempMass->next_M;
+		pTemp->pNext = new Node(data);
 	}
-
-	TempMass->next_M = new Mass(value);
-	TempMass = TempMass->next_M;
+	this->SIZE++;
 }
 
-void IntSetMassive::operator+(const IntSetMassive& object)
-{
-	//объединение множеств (типа object1 + object2), возвращает копию множества,
-	//которое является объединением двух множеств - операндов;
+void IntSetMassive::showAll() {
+	Node* pTemp = this->pHead;
+	while (pTemp != nullptr) {
+		cout << pTemp->data << " ";
+		pTemp = pTemp->pNext;
+	}
+}
 
-	Mass *TempMass1 = this->head_M;
-	Mass *TempMass2 = object.head_M;
-
-	IntSetMassive* Last = NULL;
-	
+void IntSetMassive::deleteNode(const int index) {
 	int counter = 0;
-	int* mass = new int [counter];
+	Node* pTemp = this->pHead;
 
-
-	while (true)
-	{
-		if (Last == NULL) Last = new IntSetMassive(TempMass1->data);
-		else Last->current_M->next_M = new Mass(TempMass1->data);
-		if (TempMass1->next_M == nullptr) break;
-		TempMass1 = TempMass1->next_M;
+	if (counter == index) {
+		this->pHead = this->pHead->pNext;
+		delete pTemp;
 	}
 
-	while (true)
-	{
-		if (Last == NULL) Last = new IntSetMassive(TempMass2->data);
-		else Last->current_M->next_M = new Mass(TempMass2->data);
-		if (TempMass2->next_M == nullptr) break;
-		TempMass2 = TempMass2->next_M;
-	}
+	else {
 
-
-	/*if (this == NULL) { cout << "\n\t1 множества не существует\n\t"; return; }
-	if (object.head_M == NULL && object.head_M == NULL) { cout << "\n\t2 множества не существует или оно пустое\n\t"; return; }*/
-
-	/*while (TempMass1 != nullptr)
-	{
-		counter++;
-		int *temp = new int[counter];
-		for (int i = 0; i < counter-1; i++)
-			temp[i] = mass[i];
-		delete[] mass;
-		mass = temp;
-	}
-
-	object.current_M = object.head_M;
-
-	while (true)
-	{
-		if (Last == NULL) { Last = new IntSetMassive(object.current_M->data); }
-
-		Last->current_M = new Mass(object.current_M->data);
-
-		if (object.current_M->next_M == NULL)break;
-		Last = Last->next_ISM;
-		object.current_M = object.current_M->next_M;
-	}
-
-	while (true)
-	{
-		if (fTemp->next_ISM == NULL) break;
-		fTemp = fTemp->next_ISM;
-	}
-	fTemp->next_ISM = Last;*/
-}
-
-void IntSetMassive::operator-(const int value)
-{
-	//удалить число из множества (типа object - item, где item – целое число)
-
-
-
-}
-
-void IntSetMassive::operator-(const IntSetMassive& object)
-{
-	//пересечение множеств (типа object1 - object2);
-
-
-
-}
-
-int& IntSetMassive::operator[](int value)
-{
-	//возвращает элемент по номеру;
-	int value = 0;
-
-
-
-	return value;
-}
-
-int IntSetMassive::sizeMass (const IntSetMassive*& object)
-{
-	//int () – приведение к целому, возвращает мощность множества (размер массива).
-
-
-
-}
-
-
-/*void IntSetMassive::operator+(const int value)
-{
-	while (true)
-	{
-		if (this->current_M->next_M == NULL) break;
-		this->current_M = this->current_M->next_M;
-	}
-
-	this->current_M->next_M = new Mass(value);
-	this->current_M = this->current_M->next_M;
-
-	//return object;
-}*/
-
-bool IntSetMassive::first_ISM = true;
-
-void firstGeneralTask()
-{
-	IntSetMassive* head		= NULL;
-	IntSetMassive* tail		= NULL;
-	IntSetMassive* current	= NULL;
-	bool first = true;
-	while (true)
-	{
-		cout << "\n\t1 - Добавить множество"
-			 << "\n\t2 - Добавить во множество"
-			 << "\n\t3 - Объединить множества"
-			 << "\n\t4 - Удалить число из множества"
-			 << "\n\t5 - Пересечение множеств"
-			 << "\n\t6 - Возвращение эл-та по номеру"
-			 << "\n\t7 - Мощность множества"
-			 << "\n\t0 - Выход\n\t";
-		if (first == true) cin.get();
-		char j;
-		cin >> j;
-		//system("cls");
-		int Jin = isInteger(j);
-
-		switch ((Massive_Menu)Jin)
-		{
-			case Massive_Menu::Exit: return;
-
-			case Massive_Menu::AddMass:
-			{
-				cout << "\n\tДобавление нового множества\n\tВведите размер множества: ";
-				int* arr, arr_size;
-				cin >> arr_size;
-				arr = new int[arr_size];
-
-				for (int i = 0; i < arr_size; i++)
-					cin >> arr[i];
-				if (first == true && current == NULL && head == NULL && tail == NULL)
-				{
-					current->createMassive(current, arr, arr_size);
-					head = current;
-					tail = current;
-					first = false;
-				}
-
-				else
-				{
-					current->createMassive(current, arr, arr_size);
-					tail = current;
-				}
-
-				break;
-			}
-
-			case Massive_Menu::AddItem:
-			{
-				current->getMassive(current, head, tail);
-				if (first == true) break;
-
-				cout << "\n\tВведите номер множества: ";
-				char LL;
-				cin >> LL;
-				int L = isInteger(LL);
-				L--;
-
-				cin.get();
-				cout << "\n\tВведите число: ";
-				char KK[100];
-				cin.getline(KK, 100);
-				int K = isInteger_l(KK);
-
-				current = head;
-
-				for (int i = 0; i < L; i++)
-					if (!current->currentNext(current)) break;
-
-				current + K;
-				current->getMassive(current, head, tail);
-				break;
-			}
-
-			case Massive_Menu::Combine:
-			{
-				current->getMassive(current, head, tail);
-				if (first == true) break;
-
-				cout << "\n\tВведите номер множества: ";
-				char LL;
-				cin >> LL;
-				int L = isInteger(LL);
-				L--;
-
-				cout << "\n\tВведите номер множества: ";
-				char FF;
-				cin >> FF;
-				int F = isInteger(FF);
-				F--;
-
-				IntSetMassive* temp = head;
-
-				current = head;
-
-				for (int i = 0; i < L; i++)
-					if (!current->currentNext(current)) break;
-
-				for (int i = 0; i < F; i++)
-					if (!temp->currentNext(temp)) break;
-
-				current->operator+(*temp);
-				current->getMassive(current, head, tail);
-				break;
-			}
-
-			case Massive_Menu::Remove:
-			{
-
-				break;
-			}
-
-			case Massive_Menu::Intersection:
-			{
-
-				break;
-			}
-
-			case Massive_Menu::Return:
-			{
-
-				break;
-			}
-
-			case Massive_Menu::Size:
-			{
-
-				break;
-			}
-
-			default:
-			{
-
-				break;
-			}
+		for (; counter < index - 1; counter++) {
+			pTemp = pTemp->pNext;
 		}
+
+		Node* pTempForDelete = pTemp->pNext;
+		pTemp->pNext = pTempForDelete->pNext;
+		delete pTempForDelete;
 	}
-	system("cls");
+
+	this->SIZE--;
+}
+
+void IntSetMassive::clearAll() {
+
+	for (int i = 0; i < getSize(); i++) {
+		deleteNode(i);
+	}
+
+	this->SIZE = 0;
+}
+
+bool IntSetMassive::chekValue(const int value) {
+	Node* pTemp = this->pHead;
+	while (pTemp != nullptr) {
+		if (pTemp->data == value) {
+			return true;
+		}
+		pTemp = pTemp->pNext;
+	}
+	return false;
+}
+
+void IntSetMassive::operator+(const int data) {
+	IntSetMassive::pushBack(data);
+}
+
+void IntSetMassive::operator+(const IntSetMassive& other) {
+	Node* pTemp = other.pHead;
+	while (pTemp != nullptr) {
+		IntSetMassive::pushBack(pTemp->data);
+		pTemp = pTemp->pNext;
+	}
+}
+
+void IntSetMassive::operator-(const int data) {
+	if (IntSetMassive::chekValue(data)) {
+		deleteNode(getIndexOfValue(data));
+	}
+	else {
+		cout << "none";
+	}
+}
+
+void IntSetMassive::operator-(const IntSetMassive& other) {
+	Node* pTemp = other.pHead;
+	while (pTemp != nullptr) {
+		if (IntSetMassive::chekValue(pTemp->data)) {
+			cout << pTemp->data << " ";
+		}
+		pTemp = pTemp->pNext;
+	}
+}
+
+int& IntSetMassive::operator[](const int index) {
+	Node* pTemp = this->pHead;
+
+	if (index == 0) {
+		return pTemp->data;
+	}
+
+	else {
+		int counter = 0;
+		while (counter != index) {
+			pTemp = pTemp->pNext;
+			counter++;
+		}
+		return pTemp->data;
+	}
+}
+
+int IntSetMassive::getIndexOfValue(const int value) {
+	int counter = 0;
+	Node* pTemp = this->pHead;
+	while (pTemp->data != value) {
+		pTemp = pTemp->pNext;
+		counter++;
+	}
+	return counter;
+}
+
+void firstGeneralTask() {
+	cout << "\n\t";
+	IntSetMassive it;
+	it.pushBack(2);
+	it.pushBack(-3);
+	it.pushBack(5676);
+	it.pushBack(23432);
+	IntSetMassive it2;
+	it2.pushBack(24);
+	it2.pushBack(-33);
+	it2.pushBack(56765);
+	it2.pushBack(23432);
+	it - it2;
+	it.showAll();
+	cout << "\n\t";
+	system("pause");
+	return;
 }
